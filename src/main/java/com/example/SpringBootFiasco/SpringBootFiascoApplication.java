b2bc0fde234a373c908c4714ec5b5344d727cad5
Enzo Queiroz Costa <enzo415611@gmail.com>
2025-03-12 14:03:47 -0300

    API
    Uma API de cadastro de ninjas usando springBoot

---
+++ src/main/java/com/example/SpringBootFiasco/SpringBootFiascoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootFiasco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFiascoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFiascoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootFiasco/SpringBootFiascoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootFiasco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFiascoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
